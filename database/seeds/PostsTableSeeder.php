f18d76cb25486a24408a6e738de6137ec9385d2d
manfred-commits <manfredipiraino@gmail.com>
2021-11-12 16:38:30 +0100

    Post seeder with fakers

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Post;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i <5 ; $i++) { 
+            $newPost = new Post();
+            $newPost->title = $faker->words(3, true);
+
+            $newPost->slug = Str::of($newPost->title)->slug('-');
+            $newPost->content = $faker->paragraphs();
+            $newPost->save();
+        }
+    }
+}
