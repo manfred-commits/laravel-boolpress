ce9cfdb57c939d15cbef505c3b97863cac1023b1
manfred-commits <manfredipiraino@gmail.com>
2021-11-13 11:54:17 +0100

    refactoring

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -10,8 +10,8 @@
                 <div class="card-body">
                     @foreach ($posts as $post)
                         <div class="post-container pt-2 pb-2 mb-5 d-md-flex align-items-start">
-                            <h4 class="mx-2"><strong>Title:</strong> {{$post['title']}}</h4>
-                            <h4 class="mx-2"><strong>Slug:</strong> {{$post['slug']}}</h4>
+                            <h4 class="mx-2"><strong>Title:</strong><br> {{$post['title']}}</h4>
+                            <h4 class="mx-2"><strong>Slug:</strong><br> {{$post['slug']}}</h4>
                             <h4 class="mx-2"><strong>Content:</strong> <br>{{$post['content']}}</h4>
                             <a href="{{route('admin.posts.show',$post['id'])}}" class="align-self-center">
                                 <button class="btn-primary btn px-3">Visualizza Post</button>                            
--- resources/views/admin/post/show.blade.php
+++ resources/views/admin/post/show.blade.php
@@ -5,13 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }} of {{$post['title']}}</div>
+                <div class="card-header d-flex justify-content-between">
+                    {{ __('Dashboard') }} of {{$post['title']}}
+                    <a href="{{route('admin.posts.index')}}">Back to previous section </a>
+                </div>
 
                 <div class="card-body">                    
                     <div class="post-container">
-                        <h4>Title: {{$post['title']}}</h4>
-                        <h4>Slug: {{$post['slug']}}</h4>
-                        <h4>Content: <br>{{$post['content']}}</h4>
+                        <h4><strong>Title:</strong> {{$post['title']}}</h4>
+                        <h4><strong>Slug:</strong> {{$post['slug']}}</h4>
+                        <h4><strong>Content:</strong> <br>{{$post['content']}}</h4>
+                        
                     </div>
                 </div>
                 
