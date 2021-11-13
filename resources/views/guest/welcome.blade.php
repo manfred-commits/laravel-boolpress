a647a6cc4b38fb70e808f25237a320a988a776d4
manfred-commits <manfredipiraino@gmail.com>
2021-11-13 15:23:19 +0100

    initialized front office pages with layout

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -1,146 +1,58 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.guest')
 
-        <title>{{ config('app.name', 'Boolpress') }}</title>
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('guestContent')
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            
+            @else
+                {{-- <a href="{{ route('login') }}">Login</a> --}}
 
-        <title>{{ config('app.name', 'Boolpress') }}</title>
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{route('admin.home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Boolpress
-                </div>
-                
-                <div class="links">
-                    @if (Route::has('login'))
-                        
-                        @auth
-                            
-                            
-                            <div class="dropdown show">
-                                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Actions
-                                </a>
-                                
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                    
-                                    {{-- go to logged in page link--}}
-                                    <a class="dropdown-item" href="{{route('admin.home') }}">Go to Logged in Page</a>
-
-                                    {{-- logout link --}}
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-    
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                
-                                </div>
-                            </div>
-                            <div class="">
-                                
-                            </div>
-                                
-                        @else
-                            <a href="{{ route('login') }}">Login</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}">Register</a>
-                            @endif
-                        @endauth
-                        
-                    @endif
-                </div>
-            </div>
+                {{-- @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif --}}
+            @endauth
         </div>
-    </body>
-</html>
+    @endif
+
+    <div class="content">
+        <div class="title m-b-md">
+            Boolpress
+        </div>
+        
+        <div class="links d-flex">
+            @if (Route::has('login'))
+                
+                @auth
+                    
+                <a class="dropdown-item" href="{{route('admin.home') }}">Go to Back Office</a>
+                <a class="dropdown-item" href="{{route('admin.home') }}">Go to Front Office</a>
+
+                {{-- logout link --}}
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form> 
+
+                @else
+                    <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+                    @if (Route::has('register'))
+                    <a class="dropdown-item" href="{{ route('register') }}">Register</a>
+                    @endif
+                    <a class="dropdown-item" href="{{route('admin.home') }}">Go to Front Office</a>
+                    <a class="dropdown-item" href="{{route('admin.home') }}">Go to Back Office</a>
+
+                    
+                @endauth
+                
+            @endif
+        </div>
+    </div>
+</div>
+@endsection
