3f923b667a5bc038935a11eb76949d73c8cee8ca
manfred-commits <manfredipiraino@gmail.com>
2021-11-13 18:42:57 +0100

    added front office guest post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class PostController extends Controller
 {
     public function index()
     {
-        
+        $posts= Post::all();
+        return view('guest.post.index',compact('posts'));
+    }
+    public function show($slug)
+    {
+        $post= Post::where('slug',$slug)->first();
+        if(!$post){
+            abort("404");
+        }
+        return view('guest.post.show', compact('post'));
     }
 }
