8d68aba3e3c8dc88fe2fbc0b82f8b81e2791c527
manfred-commits <manfredipiraino@gmail.com>
2021-11-13 12:39:42 +0100

    added edit &update to post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -61,9 +61,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        return view('admin.post.edit', compact('post'));
     }
 
     /**
@@ -73,9 +73,13 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $data=$request->all();
+
+        $data['slug']=Str::of($data['title'])->slug('-');
+        $post->update($data);
+        return redirect()->route('admin.posts.show',compact('post'));
     }
 
     /**
