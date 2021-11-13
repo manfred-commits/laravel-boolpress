85f5a594149fb774ac555550c7ce8d47e3263e2c
manfred-commits <manfredipiraino@gmail.com>
2021-11-13 10:58:19 +0100

    refactoring dashboard

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12 col-xs-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }} of all posts</div>
 
                 <div class="card-body">
                     @foreach ($posts as $post)
-                        <div class="post-container pt-2 pb-2 mb-5">
-                            <h4>Title: {{$post['title']}}</h4>
-                            <h4>Slug: {{$post['slug']}}</h4>
-                            <h4>Content: <br>{{$post['content']}}</h4>
-                            <a href="{{route('admin.posts.show',$post['id'])}}">
-                                <button class="btn-primary btn">Visualizza Singolo Post</button>                            
+                        <div class="post-container pt-2 pb-2 mb-5 d-md-flex align-items-start">
+                            <h4 class="mx-2"><strong>Title:</strong> {{$post['title']}}</h4>
+                            <h4 class="mx-2"><strong>Slug:</strong> {{$post['slug']}}</h4>
+                            <h4 class="mx-2"><strong>Content:</strong> <br>{{$post['content']}}</h4>
+                            <a href="{{route('admin.posts.show',$post['id'])}}" class="align-self-center">
+                                <button class="btn-primary btn px-3">Visualizza Post</button>                            
                             </a>
                         </div>
                     @endforeach
