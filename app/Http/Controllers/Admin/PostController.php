c993b79b69dba8529eea025e901e77e7cc02ec29
manfred-commits <manfredipiraino@gmail.com>
2021-11-17 16:02:02 +0100

    added category_id in creation

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Post;
+use App\Category;
 class PostController extends Controller
 {
 
@@ -32,8 +33,9 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-    {
-        return view('admin.posts.create');
+    {   
+        $categories= Category::all();
+        return view('admin.posts.create',compact("categories"));
     }
 
     /**
