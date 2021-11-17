a9dc34de520c17f99fcfe28299412a6048fce67b
manfred-commits <manfredipiraino@gmail.com>
2021-11-17 15:46:51 +0100

    added entity association method

--- app/Category.php
+++ app/Category.php
@@ -5,6 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
-{
-    //
+{   
+    protected $fillable = ['name'];
+
+    // method with the entity with which we need to establish a relation
+    public function posts(){
+        return $this->hasMany("App/Post");
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'slug', 'content'];
+    
+    // method with the entity with which we need to establish a relation
+    public function category(){
+        return $this->belongsTo("App/Category");
+    }
 }
