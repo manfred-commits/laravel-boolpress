03a860f9c988bd63acf3200de286a1d5334b3dbf
manfred-commits <manfredipiraino@gmail.com>
2021-11-18 16:44:27 +0100

    added columns in migration

--- database/migrations/2021_11_18_153825_create_tags_table.php
+++ database/migrations/2021_11_18_153825_create_tags_table.php
@@ -15,6 +15,8 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name',50)->unique();
+            $table->string('slug',50)->unique();
             $table->timestamps();
         });
     }
