aa17e8219f0354b1ddfc4223ce782384f6bb2d40
manfred-commits <manfredipiraino@gmail.com>
2021-11-22 17:44:28 +0100

    added components of App.vue

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        main
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'Main',
+        data(){
+            postsList,
+        }
+    }
+</script>
