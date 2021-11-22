48da610814a3ff18bf4711b7d741cd173ee2bcc3
manfred-commits <manfredipiraino@gmail.com>
2021-11-22 17:54:06 +0100

    added dynamic print of posts titles through api

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,6 +1,11 @@
 <template>
     <main>
-        main
+        <h2>Posts Titles</h2>
+        <ul>
+            <li v-for="post in postsList" :key="post.id">
+                {{post.title}}
+            </li>
+        </ul>
     </main>
 </template>
 
@@ -8,7 +13,20 @@
     export default {
         name: 'Main',
         data(){
-            postsList,
+            return{
+                postsList: [],
+
+            }
+        },
+        mounted(){
+            axios.get('api/posts')
+            .then((response) => {
+                this.postsList=response.data.data;
+                // console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
         }
     }
 </script>
